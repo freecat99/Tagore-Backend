5b120a5db1dc0cbc197af45f42fb4a8a0c43e18c
Pratham Raj <praj79074@gmail.com>
2025-12-18 19:33:59 +0530

    Added 'cors' package to allow requests from localhost:5173

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import authRouter from './Routes/authRouter.js';
 import { connectDB } from './Lib/db.js';
-import paymentRouter from './Routes/paymentRouter.js';
-import articleRouter  from './Routes/articleRouter.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173", // Frontend URL
+    credentials: true // allows the cookie to pass
+}));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
-app.use('/api/payment', paymentRouter);
-app.use('/api/article', articleRouter);
 
 
 const PORT = process.env.PORT || 1600;
